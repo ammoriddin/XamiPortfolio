052bb0f5d86b0153b999f6399e693934ef561479
ammoriddin <hakimjavoh@gmail.com>
2022-11-03 12:31:10 +0600

    Finish

--- js/app.js
+++ js/app.js
@@ -2,3 +2,13 @@ window.addEventListener('scroll',() => {
     let navScroll = document.querySelector("#nav");
     navScroll.classList.toggle("fixed", window.scrollY > 10);
 })
+
+function navBack(){
+    let nav = document.getElementById('nav').classList.toggle("change");
+    let getCollaps = document.querySelector(".collapse")
+    if (getCollaps.style.display == 'block'){
+        getCollaps.style.display = 'none'
+    }else{
+        getCollaps.style.display = 'block'
+    }
+}
